f99d897b16d2f5e48d669e28fcdcaac9bc282c9c
saketb-2703 <79461608+saketb-2703@users.noreply.github.com>
2023-04-24 00:12:32 +0530

    Update symbol.cc

--- src/symbol.cc
+++ src/symbol.cc
@@ -4,6 +4,22 @@ bool SymbolTable::contains(std::string key) {
     return table.find(key) != table.end();
 }
 
-void SymbolTable::insert(std::string key) {
-    table.insert(key);
+void SymbolTable::insert(std::string key, int value) {
+    table[key] = value;
+}
+
+int SymbolTable::get(std::string key) {
+    return table[key];
+}
+
+bool FunctionSymbolTable::contains(std::string key) {
+    return table.find(key) != table.end();
+}
+
+void FunctionSymbolTable::insert(std::string key, std::vector<Node*> nodes) {
+    table[key] = nodes;
+}
+
+std::vector<Node*> FunctionSymbolTable::get(std::string key) {
+    return table[key];
 }
